6ce4b32c83a2cb776c201b8c80d769a908553bc9
pedro <zpedroluis@outlook.com>
2020-07-22 15:38:46 -0300

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,24 @@
+import { Oferta } from './../shared/oferta.model';
 import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
+
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Array<Oferta>
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    let ofertas: OfertasService = new OfertasService()
-    console.log(ofertas.getOfertas())
+    this.ofertas = this.ofertasService.getOfertas()
+    console.log(this.ofertas)
   }
 
 }
